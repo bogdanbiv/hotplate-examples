a8a80399c702f353604d19cd4516723d244c6b5d
Tony Mobily <tonymobily@gmail.com>
2013-09-16 16:24:53 +0800

    First example up

--- app.js
+++ app.js
@@ -6,25 +6,44 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var hotplate = require('hotplate');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+// Associate "app" to hotplate
+hotplate.setApp( app );
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+// Register two Hotplate modules
+hotplate.registerModule( 'module1', require('module1') );
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+// Initialise the modules. Once done, continue with node's usual rock&roll
+hotplate.initModules( function() {
+
+  // all environments
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // development only
+  if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+  }
+
+  // Run the modules. This will call `run` of each module. `run` can do all sorts
+  // of fancy things, like setting routes etc.
+  hotplate.runModules( function() {
+
+    http.createServer(app).listen(app.get('port'), function(){
+      console.log('Express server listening on port ' + app.get('port'));
+    });
+  });
+
+}) // End of the rock&roll
+
+
